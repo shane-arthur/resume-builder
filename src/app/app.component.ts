cfe3f991eb072f969e33fe1557710156e7a58b81
Shane Arthur <shane.arthur@gmail.com>
2020-05-14 14:05:16 -0400

    intermittent checkin.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'resume-builder';
+
+  ngAfterViewInit(){
+    //const filename  = 'ThisIsYourPDFFilename.pdf';
+
+		html2canvas(document.body, 
+								{scale: 1}
+						 ).then(canvas => {
+               document.body.appendChild(canvas);
+			// let pdf = new jsPDF('p', 'mm', 'a4');
+			// pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+			// pdf.save(filename);
+		});
+  }
+
 }

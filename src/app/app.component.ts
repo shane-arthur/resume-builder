c9a95d0c638e51d0602cb076e082108713baef80
Shane Arthur <shane.arthur@gmail.com>
2020-05-14 16:18:03 -0400

    intermittent checkin resume rendering.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,15 @@ export class AppComponent implements AfterViewInit {
   title = 'resume-builder';
 
   ngAfterViewInit(){
-    //const filename  = 'ThisIsYourPDFFilename.pdf';
+    const filename  = 'shane-arthur-may-2020.pdf';
 
 		html2canvas(document.body, 
-								{scale: 1, allowTaint: true,  useCORS: true}
+								{scale: 1, allowTaint: true,   imageTimeout:0,  useCORS: true}
 						 ).then(canvas => {
                document.body.appendChild(canvas);
-			// let pdf = new jsPDF('p', 'mm', 'a4');
-			// pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
-			// pdf.save(filename);
+			let pdf = new jsPDF('p', 'mm', 'a4');
+			pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+			pdf.save(filename);
 		});
   }
 

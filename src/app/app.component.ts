401bf32be083cb5f44b43667e7563f78e5a5d149
Shane Arthur <shane.arthur@gmail.com>
2020-05-14 20:38:19 -0400

    Two pages seperation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,35 @@ import jsPDF from 'jspdf';
 export class AppComponent implements AfterViewInit {
   title = 'resume-builder';
 
-  ngAfterViewInit(){
-    const filename  = 'shane-arthur-may-2020.pdf';
+  ngAfterViewInit() {
+    const filename = 'shane-arthur-may-2020.pdf';
 
-		html2canvas(document.body, 
-								{scale: 1, allowTaint: true,   imageTimeout:0,  useCORS: true}
-						 ).then(canvas => {
-               console.log(canvas);
-               document.body.appendChild(canvas);
-      let pdf = new jsPDF('l');
+    const pdf = new jsPDF('l', 'in', [2000, 1500]);
+    html2canvas(document.getElementById('firstPage'),
+      { scale: 2, allowTaint: true, imageTimeout: 0, useCORS: true }
+    ).then(canvas => {
       const imgData = canvas.toDataURL('image/png');
-      const imgProps= pdf.getImageProperties(imgData);
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+
+      html2canvas(document.getElementById('secondPage'),
+        { scale: 1, allowTaint: true, imageTimeout: 0, useCORS: true }
+      ).then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+        const imgProps = pdf.getImageProperties(imgData);
         const pdfWidth = pdf.internal.pageSize.getWidth();
         const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-			pdf.save(filename);
-		});
+        pdf.addPage();
+        pdf.setPage(2);
+        pdf.addImage(imgData, 'PNG', 0, 1, pdfWidth, pdfHeight);
+        pdf.save(filename);
+      });
+
+    });
+
   }
 
 }

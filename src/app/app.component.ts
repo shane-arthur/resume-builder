8d27963b267a281855b84d6e3e99c537fec938e1
Shane Arthur <shane.arthur@gmail.com>
2020-05-21 09:27:09 -0400

    Uncomment resume builder func.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,35 +11,35 @@ export class AppComponent implements AfterViewInit {
   title = 'resume-builder';
 
   ngAfterViewInit() {
-    // const filename = 'shane-arthur-may-2020.pdf';
+    const filename = 'shane-arthur-may-2020.pdf';
 
-    // const pdf = new jsPDF('l', 'in', [2000, 1600]);
-    // html2canvas(document.getElementById('firstPage'),
-    //   { scale: 2, allowTaint: true, imageTimeout: 0, useCORS: true }
-    // ).then(canvas => {
-    //   const imgData = canvas.toDataURL('image/png');
-    //   const imgProps = pdf.getImageProperties(imgData);
-    //   const pdfWidth = pdf.internal.pageSize.getWidth();
-    //   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    const pdf = new jsPDF('l', 'in', [2000, 1600]);
+    html2canvas(document.getElementById('firstPage'),
+      { scale: 2, allowTaint: true, imageTimeout: 0, useCORS: true }
+    ).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-    //   pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
 
-    //   html2canvas(document.getElementById('secondPage'),
-    //     { scale: 2, allowTaint: true, imageTimeout: 0, useCORS: true }
-    //   ).then(canvas => {
-    //     var context = canvas.getContext("2d");
-    //     context.fillStyle="#FFFFFF";
-    //     const imgData2 = canvas.toDataURL('image/png');
-    //     const imgProps = pdf.getImageProperties(imgData2);
-    //     const pdfWidth = pdf.internal.pageSize.getWidth();
-    //     const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-    //     pdf.addPage();
-    //     pdf.setPage(2);
-    //     pdf.addImage(imgData2, 'PNG', 0, 0, pdfWidth, pdfHeight);
-    //     pdf.save(filename);
-    //   });
+      html2canvas(document.getElementById('secondPage'),
+        { scale: 2, allowTaint: true, imageTimeout: 0, useCORS: true }
+      ).then(canvas => {
+        var context = canvas.getContext("2d");
+        context.fillStyle = "#FFFFFF";
+        const imgData2 = canvas.toDataURL('image/png');
+        const imgProps = pdf.getImageProperties(imgData2);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        pdf.addPage();
+        pdf.setPage(2);
+        pdf.addImage(imgData2, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        pdf.save(filename);
+      });
 
-    // });
+    });
 
   }
 
